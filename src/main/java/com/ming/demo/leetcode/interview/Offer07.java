f1e2e4cc993286a41beebc0d20d1463b3b0b61cb
Ming <weixm@visvachina.com>
2021-10-09 18:16:57 +0800

    重建二叉树算法题，待做完

---
+++ src/main/java/com/ming/demo/leetcode/interview/Offer07.java
@@ -0,0 +1,31 @@
+package com.ming.demo.leetcode.interview;
+
+import java.util.HashMap;
+
+/**
+ * @author Ming
+ * @date 2021/10/9 17:10
+ */
+public class Offer07 {
+
+	// todo:待完善
+	int[] preorder;
+	HashMap<Integer, Integer> dic = new HashMap<>();
+
+
+	/**
+	 * 重建二叉树
+	 * <p>题目要求：输入某二叉树的前序遍历和中序遍历的结果，请构建该二叉树并返回其根节点。
+	 *
+	 * 假设输入的前序遍历和中序遍历的结果中都不含重复的数字。</p>
+	 */
+	public TreeNode buildTree(int[] preorder, int[] inorder) {
+
+		return null;
+	}
+
+	TreeNode recur(int root,int left,int right){
+
+		return null;
+	}
+}
