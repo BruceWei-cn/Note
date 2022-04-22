b2e129d1583a1a50678009163d8c768c75f6416b
Ming <xiaomingwei@trip.com>
2022-04-22 16:44:42 +0800

    批量异步操作需要借助JUC工具类做栅栏

--- src/main/java/com/ming/demo/CompleteFutureTest.java
+++ src/main/java/com/ming/demo/CompleteFutureTest.java
@@ -2,8 +2,10 @@ package com.ming.demo;
 
 import org.junit.Test;
 
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.*;
 
 /**
  * @author Ming
@@ -26,5 +28,44 @@ public class CompleteFutureTest {
         System.out.println("async = " + async.get());
         Thread.sleep(3500);
         System.out.println("主线程结束");
+    }
+
+
+    /**
+     * 异步 多线程的使用在批量运行获取最终结果时，还是需要一些计数器来做额外控制
+     *
+     * @throws InterruptedException
+     */
+    @Test
+    public void demo02() throws InterruptedException {
+        int ava = Runtime.getRuntime().availableProcessors();
+        System.out.println("ava = " + ava);
+        ThreadPoolExecutor executor = new ThreadPoolExecutor(Runtime.getRuntime().availableProcessors(), 100, 5,
+                TimeUnit.SECONDS, new LinkedBlockingQueue<>(), new ThreadPoolExecutor.DiscardPolicy());
+        List<Integer> list = Collections.synchronizedList(new ArrayList<>());
+        CountDownLatch latch = new CountDownLatch(100);
+        for (int i = 0; i < 100; i++) {
+            CompletableFuture.runAsync(() -> {
+                list.add(getNum());
+                latch.countDown();
+            }, executor);
+        }
+        latch.await();
+        System.out.println("list.size() = " + list.size());
+
+        List<Integer> list2 = Collections.synchronizedList(new ArrayList<>());
+        CountDownLatch latch2 = new CountDownLatch(100);
+        for (int i = 0; i < 100; i++) {
+            executor.execute(() -> {
+                list2.add(getNum());
+                latch2.countDown();
+            });
+        }
+        latch2.await();
+        System.out.println("list2.size() = " + list2.size());
+    }
+
+    private int getNum() {
+        return 1;
     }
 }
